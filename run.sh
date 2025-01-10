282718d4c987a1c330b0f55bb2efa5bd91ec54f0
Yannick Molinghen <yannick.molinghen@ulb.be>
2025-01-10 11:03:08 +0000

    results

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-envs=("shaped-lle-no-reward")
-configs=("vdn-cnn")
-n_seeds=8
-n_concurrent=16
+envs=("lle-lvl6")
+configs=("maser-cnn")
+n_seeds=20
+n_concurrent=8
 
 j=0
 for env in ${envs[@]}; do
     for config in ${configs[@]}; do
-        for seed in $(seq 4 11); do
+        for seed in $(seq 0 ${n_seeds}); do
             echo "Starting job number ${j}: ${config} ${env} seed ${seed}"
             python src/main.py --config=${config} --env-config=${env} with env_args.seed=${seed} &
             if [ $(jobs -p | wc -l) -ge ${n_concurrent} ]; then
