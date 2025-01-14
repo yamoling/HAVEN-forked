148689d6a3fdcd7ec55462f4df403ba37467652b
Yannick Molinghen <yannick.molinghen@ulb.be>
2025-01-14 11:23:06 +0100

    Ready to run

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #configs[0]="--config=maser-cnn --env-config=lle-lvl6"
-configs[0]="--config=vdn-cnn --env-config=shaped-lle"
-n_seeds=8
-n_concurrent=8
+configs[0]="--config=qmix-cnn --env-config=shaped-lle"
+n_seeds=16
+n_concurrent=16
 
 j=0
 for config in "${configs[@]}"; do
-    for seed in $(seq 8 15); do
+    for seed in $(seq 0 $(($n_seeds-1))); do
         echo "Starting job number ${j}: ${config} with seed=${seed}"
         python src/main.py ${config} with env_args.seed=${seed} &
         if [ $(jobs -p | wc -l) -ge ${n_concurrent} ]; then
