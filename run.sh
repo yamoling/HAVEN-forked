72c338fa63d8d32feab74dbbe6678b876ea75c85
Yannick Molinghen <yannick.molinghen@ulb.be>
2025-01-16 10:10:04 +0000

    Results

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ n_concurrent=16
 
 j=0
 for config in "${configs[@]}"; do
-    for seed in $(seq 0 $(($n_seeds-1))); do
+    for seed in $(seq $n_seeds $(($n_seeds+$n_seeds-1))); do
         echo "Starting job number ${j}: ${config} with seed=${seed}"
         python src/main.py ${config} with env_args.seed=${seed} &
         if [ $(jobs -p | wc -l) -ge ${n_concurrent} ]; then
--- src/learners/q_learner.py
+++ src/learners/q_learner.py
@@ -109,7 +109,7 @@ class QLearner(Learner):
 
         if t_env - self.log_stats_t >= self.args.learner_log_interval:
             self.logger.log_stat("loss", float(loss.item()), t_env)
-            self.logger.log_stat("grad_norm", float(grad_norm), t_env)
+            self.logger.log_stat("grad_norm", float(grad_norm.item()), t_env)
             mask_elems = mask.sum().item()
             self.logger.log_stat("td_error_abs", float((masked_td_error.abs().sum().item() / mask_elems)), t_env)
             self.logger.log_stat(
