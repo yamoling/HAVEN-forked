228a31f84a90e7fc112d850d6424fe262d7df948
Yannick Molinghen <yannick.molinghen@ulb.be>
2025-01-18 14:26:11 +0000

    MASER results

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#configs[0]="--config=maser-cnn --env-config=lle-lvl6"
+configs[0]="--config=maser-cnn --env-config=shaped-lle"
 #configs[0]="--config=qmix-cnn --env-config=shaped-lle"
-configs[0]="--config=qplex --env-config=lle-lvl6"
-configs[1]="--config=qplex --env-config=shaped-lle"
-n_seeds=8
-n_concurrent=16
+#configs[0]="--config=qplex --env-config=lle-lvl6"
+#configs[1]="--config=qplex --env-config=shaped-lle"
+n_seeds=16
+n_concurrent=8
 
 j=0
 for config in "${configs[@]}"; do
-    for seed in $(seq $n_seeds $(($n_seeds+$n_seeds-1))); do
+    for seed in $(seq 0 $(($n_seeds-1))); do
         echo "Starting job number ${j}: ${config} with seed=${seed}"
         python src/main.py ${config} with env_args.seed=${seed} &
         if [ $(jobs -p | wc -l) -ge ${n_concurrent} ]; then
